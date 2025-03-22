ca46e4585237fe3a6c4d5eb0dd6a6e35924a6d9b
Ivan21T <119493817+Ivan21T@users.noreply.github.com>
2025-03-22 10:25:18 +0200

    Proxy

---
+++ Proxy/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    internal class BankAccount : IAccount
+    {
+        public decimal Balance { get; set; }
+        public BankAccount(decimal balance)
+        {
+            Balance = balance;
+        }
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Amount cannot be negative number or zero");
+            }
+            else if (amount > Balance)
+            {
+                Console.WriteLine("Not enough money in the balance");
+            }
+            else
+            {
+                Balance -= amount;
+                Console.WriteLine("Remaining money in the balance:{0}",Balance);
+            }
+        }
+    }
+}
---
+++ Proxy/Check.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    internal class Check : IAccount
+    {
+        private decimal amount;
+        private string authentificationPhrase;
+        private BankAccount bankAccount;
+        public Check(decimal amount, BankAccount bankAccount,string phrase)
+        {
+            this.amount = amount;
+            this.bankAccount = bankAccount;
+            this.authentificationPhrase = phrase;
+        }
+        public void Withdraw(decimal amount)
+        {
+            Console.Write("Enter the authentification phrase:");
+            string phrase=Console.ReadLine();
+            if (phrase == authentificationPhrase)
+            {
+                bankAccount.Withdraw(amount);
+            }
+            else
+            {
+                Console.WriteLine("Cannot withdraw money from the balance");
+            }
+        }
+    }
+}
