ac488113b786b5beac61237c4efffed54e2f89b6
Ivan21T <119493817+Ivan21T@users.noreply.github.com>
2025-03-22 10:40:02 +0200

    Interpreter

---
+++ Interpreter/InterpreterClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    public static class InterpreterClass
+    {
+        public static void Аdd(double a,double b)
+        {
+            Console.WriteLine("{0} + {1} = {2}",a,b,a+b);
+        }
+        public static void Subtraction(double a,double b)
+        {
+            Console.WriteLine("{0} - {1} = {2}",a,b,a-b);
+        }
+        public static void Multiplication(double a,double b)
+        {
+            Console.WriteLine("{0} x {1} = {2}",a,b,a*b);
+        }
+        public static void Division(double a,double b)
+        {
+            Console.WriteLine("{0} / {1} = {2}",a,b,a/b);
+        }
+    }
+}
---
+++ Interpreter/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Number 1: ");
+                double a = double.Parse(Console.ReadLine());
+                Console.Write("Number 2: ");
+                double b = double.Parse(Console.ReadLine());
+                Console.Write("Action: ");
+                string answer = Console.ReadLine();
+                if (answer.ToLower()=="add" || answer.ToLower()=="събиране")
+                {
+                    InterpreterClass.Аdd(a, b);
+                }
+                else if (answer.ToLower()=="division" || answer.ToLower()=="делене")
+                {
+                    InterpreterClass.Division(a, b);
+                }
+                else if (answer.ToLower() == "изваждане" || answer.ToLower() == "subtraction")
+                {
+                    InterpreterClass.Subtraction(a, b);
+                }
+                else if (answer.ToLower() == "умножение" || answer.ToLower() == "multiplication")
+                {
+                    InterpreterClass.Multiplication(a, b);
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
