5056b623b96335c18c6af13e6ad62b655a60a1ce
Ivan21T <119493817+Ivan21T@users.noreply.github.com>
2025-03-22 10:27:45 +0200

    FlyWeight

---
+++ Flyweight/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    internal class Car
+    {
+        public int HorsePower { get; set; }
+        public string Color {  get; set; }
+        public string Number {  get; set; }
+        public Car(int horsePower,string color,string number) 
+        {
+            HorsePower = horsePower;
+            Color = color;
+            Number = number;
+        }
+        public void Drive()
+        {
+            Console.WriteLine("I drive {0} car with {1} hp, number: {2}",Color,HorsePower,Number);
+        }
+    }
+}
---
+++ Flyweight/CarFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    internal class CarFactory
+    {
+        private static Dictionary<string, Car> cars = new Dictionary<string, Car>();
+        public static Car GetCar(string number,int hp,string color)
+        {
+            if (!cars.ContainsKey(number))
+            {
+                Car car=new Car(hp,color,number);
+                cars.Add(number,car);
+                Console.WriteLine("Successful created new car");
+                return car;
+            }
+            Console.WriteLine("Find searched car in the collection");
+            return cars[number];
+        }
+
+    }
+}
